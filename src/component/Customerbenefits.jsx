22276cf75f4da6aeff14cba344192bf120036721
God's time Maklu <godstimemaklu3@gmail.com>
2025-01-24 17:01:45 +0100

    responsiveness on cus benefit

--- src/component/Customerbenefits.jsx
+++ src/component/Customerbenefits.jsx
@@ -4,7 +4,7 @@ const CustomerBenefits = () => {
   return (
     <div className="flex flex-wrap justify-between items-center p-6 bg-white border border-gray-200 rounded-lg shadow-sm mt-32">
       {/* Free Shipping */}
-      <div className="flex flex-col items-center text-center w-1/4 px-4">
+      <div className="flex flex-col items-center text-center w-full sm:w-1/2 lg:w-1/4 px-4 mb-6 sm:mb-0">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-12 h-12 text-gray-600">
           <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 15h16.5M3.75 15a2.25 2.25 0 01-2.25-2.25V8.25A2.25 2.25 0 013.75 6h12a2.25 2.25 0 012.25 2.25v4.5a2.25 2.25 0 012.25 2.25m-18 0v1.5a2.25 2.25 0 002.25 2.25h15a2.25 2.25 0 002.25-2.25V15m-4.5 0v1.5m-12 0v1.5m9-1.5h.008v.008H15v-.008z" />
         </svg>
@@ -13,7 +13,7 @@ const CustomerBenefits = () => {
       </div>
 
       {/* Secure Payment */}
-      <div className="flex flex-col items-center text-center w-1/4 px-4">
+      <div className="flex flex-col items-center text-center w-full sm:w-1/2 lg:w-1/4 px-4 mb-6 sm:mb-0">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-12 h-12 text-gray-600">
           <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 9.75a3.75 3.75 0 00-7.5 0v1.5h7.5v-1.5zM9 12a3 3 0 013-3h0a3 3 0 013 3v6a3 3 0 01-6 0v-6z" />
         </svg>
@@ -22,7 +22,7 @@ const CustomerBenefits = () => {
       </div>
 
       {/* Support 24/7 */}
-      <div className="flex flex-col items-center text-center w-1/4 px-4">
+      <div className="flex flex-col items-center text-center w-full sm:w-1/2 lg:w-1/4 px-4 mb-6 sm:mb-0">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-12 h-12 text-gray-600">
           <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 15.75a3.75 3.75 0 116.75 0v3.75h-6.75v-3.75zm0 3.75h6.75v3.75m0-3.75v3.75" />
         </svg>
@@ -31,7 +31,7 @@ const CustomerBenefits = () => {
       </div>
 
       {/* 30 Days Return */}
-      <div className="flex flex-col items-center text-center w-1/4 px-4">
+      <div className="flex flex-col items-center text-center w-full sm:w-1/2 lg:w-1/4 px-4">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-12 h-12 text-gray-600">
           <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 15h16.5m-16.5 0a2.25 2.25 0 01-2.25-2.25v-4.5A2.25 2.25 0 013.75 6h16.5a2.25 2.25 0 012.25 2.25v4.5A2.25 2.25 0 0119.75 15M6.75 6.75L12 12m0 0l5.25-5.25M12 12v6" />
         </svg>
