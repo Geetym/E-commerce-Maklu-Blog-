950b019216bc1b729aaa1d963af518d1dcbea464
God's time Maklu <godstimemaklu3@gmail.com>
2025-01-24 16:10:47 +0100

    24th fourth push

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,9 @@ import Castronews from './component/Castronews';
 import CustomerBenefits from './component/Customerbenefits';
 import Footersection from './component/Footersection';
 import MastercardPay from './component/Mastercardpay';
+import Vervecardplay from './component/Vervecardplay';
+import Visacard from './component/Visacard';
+import Americanexpress from './component/Americanexpress';
 
 const App = () => {
   return (
@@ -33,7 +36,9 @@ const App = () => {
         <Route path="/loginpage" element={<LoginPage />} />
         <Route path='/viewallproducts' element={<Allproduct />} />
         <Route path='/mastercard' element={<MastercardPay />} />
-        
+        <Route path='/vervecard' element={<Vervecardplay />} />
+        <Route path='/visacard' element={<Visacard />} />
+        <Route path='/americanexpresscard' element={<Americanexpress />} />
       </Routes>
     </>
   );
